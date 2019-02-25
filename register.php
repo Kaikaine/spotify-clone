226d7a91aef9151d4bee257fcfdd9785fc9eb543
kaikaine <kairijb@protonmail.com>
2019-02-25 07:17:28 -0600

    added functions to sanitize user input for registration

--- register.php
+++ register.php
@@ -1,3 +1,46 @@
+<?php 
+
+function sanitizeFormPassword($inputText) {
+	$inputText = strip_tags($inputText);
+	return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	$inputText = ucfirst(strtolower($inputText));
+	return $inputText;
+}
+
+if(isset($_POST['loginButton'])) {
+
+}
+
+if(isset($_POST['registerButton'])) {
+    //Register button was pressed
+	$username = sanitizeFormUsername($_POST['username']);
+	
+	$firstName = sanitizeFormString($_POST['firstName']);
+
+	$lastName = sanitizeFormString($_POST['lastName']);
+
+	$email = sanitizeFormString($_POST['email']);
+
+	$email2 = sanitizeFormString($_POST['email2']);
+
+	$password = sanitizeFormPassword($_POST['password']);
+
+	$password2 = sanitizeFormPassword($_POST['password2']);
+}
+
+?>
+
 <html>
 <head>
 	<title>Welcome to Slotify!</title>
@@ -24,24 +67,24 @@
         <form id="registerForm" action="register.php" method="POST">
 			<h2>Create your free account</h2>
 			<p>
-				<label for="Username">Username</label>
-				<input id="username" name="Username" type="text" placeholder="Username" required>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" required>
 			</p>
             <p>
 				<label for="firsName">First Name</label>
-				<input id="firstName" name="firsName" type="text" placeholder="First Name" required>
+				<input id="firstName" name="firsName" type="text" required>
 			</p>
             <p>
 				<label for="lastName">Last Name</label>
-				<input id="lastName" name="lastName" type="text" placeholder="Last Name" required>
+				<input id="lastName" name="lastName" type="text" required>
 			</p>
             <p>
 				<label for="email">Email</label>
-				<input id="email" name="email" type="text" placeholder="Email" required>
+				<input id="email" name="email" type="text" required>
 			</p>
             <p>
 				<label for="email2">Confirm Email</label>
-				<input id="email2" name="email2" type="text" placeholder="Email" required>
+				<input id="email2" name="email2" type="text" required>
 			</p>
 			<p>
 				<label for="password">Password</label>
