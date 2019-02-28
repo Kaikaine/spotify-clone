d6be9316dacb320c1d0ff009b420ec43c6beeb2c
kaikaine <kairijb@protonmail.com>
2019-02-28 01:53:39 -0600

    created sessions

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<!DOCTYPE html>
+<?php
+include("includes/config.php");
+
+//session_destroy(); LOGOUT
+
+if(isset($_SESSION['userLoggedIn'])) {
+	$userLoggedIn = $_SESSION['userLoggedIn'];
+}
+else {
+	header("Location: register.php");
+}
+
+?>
+
 <html>
 <head>
     <meta charset="utf-8">
