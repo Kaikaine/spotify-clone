c91bdd66568dd0cdbbc7186fba008cd743f7158c
kaikaine <kairijb@protonmail.com>
2019-02-28 10:52:40 -0600

    started on control panel; added icons for control panel

--- index.php
+++ index.php
@@ -16,12 +16,51 @@ else {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Welcome to Slotify</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css">
     <script src="main.js"></script>
 </head>
 <body>
-    Hello
+
+   <div class="nowPlayingBarContainer">
+        <div class="nowPlayingBar">
+            <div class="nowPlayingLeft">
+
+            </div>
+            <div class="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class='controlButton shuffle' title='Shuffle button'>
+                            <img src="assets/images/icons/shuffle.png" alt="shuffle">
+                        </button>
+
+                        <button class='controlButton previous' title='previous button'>
+                            <img src="assets/images/icons/previous.png" alt="shuffle">
+                        </button>
+
+                        <button class='controlButton play' title='play button'>
+                            <img src="assets/images/icons/play.png" alt="shuffle">
+                        </button>
+
+                        <button style='display: none' class='controlButton pause' title='pause button'>
+                            <img src="assets/images/icons/pause.png" alt="shuffle">
+                        </button>
+
+                        <button class='controlButton next' title='next button'>
+                            <img src="assets/images/icons/next.png" alt="shuffle">
+                        </button>
+
+                        <button class='controlButton repeat' title='repeat button'>
+                            <img src="assets/images/icons/repeat.png" alt="repeat">
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="nowPlayingRight">
+
+            </div>
+        </div>
+   </div>
 </body>
 </html>
